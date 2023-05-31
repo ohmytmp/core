e059203fb07fed9f6e0acd3fa694aba61df1ee4e
Elaina <userelaina@pm.me>
2023-05-31 07:24:44 +0000

    :white_check_mark: update `test.py`

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
-from ohmytmp import Ohmytmp, Info, PluginAfter
+from ohmytmp import Ohmytmp, Info, PluginAfter, FUNC, VERSION
 
-def prt(i: Info)->None:
-    print(i.to_dict())
+print(VERSION)
 
 a = Ohmytmp()
+
+
+def prt(i: Info) -> None:
+    print(i.to_dict())
+
+
+class CC:
+    def __init__(self) -> None:
+        pass
+    @a.reg_handle(FUNC.ADDTAGS, -1)
+    def f(info):
+        print(FUNC.ADDTAGS, '_handle_f')
+    def g(self, info: Info):
+        print(FUNC.DESTINATION, '_f_g')
+
+
 a.register(PluginAfter(prt))
+a.reg_f(CC().g, FUNC.DESTINATION)
+
 a.walk('./')
