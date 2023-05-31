1ec5432819c651420444754df5f728babdf96c82
Elaina <userelaina@pm.me>
2023-05-31 07:31:38 +0000

    :sparkles: add `__all__` to `__init__.py`

--- src/ohmytmp/__init__.py
+++ src/ohmytmp/__init__.py
@@ -1,8 +1,11 @@
 from .__constant import *
-del __constant
-from .__core import *
-del __core
 from .__plugin import *
+from .__core import *
+
+del __constant
 del __plugin
+del __core
 
 VERSION = '0.0.6'
+
+__all__ = ('FUNC', 'TYPE', 'Info', 'PluginBase', 'Ohmytmp')
